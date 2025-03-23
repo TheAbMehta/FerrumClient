4144eb4aa60da7fc99afed19b603696898d9e3e6
Ferrum Dev <dev@ferrum.rs>
2025-03-23 16:13:07 +0000

    feat(protocol): add type aliases for common Minecraft packets

--- ferrum-protocol/src/lib.rs
+++ ferrum-protocol/src/lib.rs
@@ -1,1 +1,4 @@
-
+pub use azalea_protocol::packets::config::ClientboundConfigPacket as ConfigPacket;
+pub use azalea_protocol::packets::game::c_level_chunk_with_light::ClientboundLevelChunkWithLight as ChunkDataPacket;
+pub use azalea_protocol::packets::game::ClientboundGamePacket as GamePacket;
+pub use azalea_protocol::packets::login::ClientboundLoginPacket as LoginPacket;
